5c1dfaa98136279c2b5e049dce19e10b16100397
laksnthan-droid <laksn.than@bumail.net>
2026-03-17 02:35:25 +0700

    save

---
+++ Assets/Scenes/Test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+Rigidbody rb;
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
