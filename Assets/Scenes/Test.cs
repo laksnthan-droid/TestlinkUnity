e4dff328cc7ccc8f5190576c9a5fd529cd93e7a8
laksnthan-droid <laksn.than@bumail.net>
2026-03-17 03:08:30 +0700

    save

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -1,19 +1,50 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-Rigidbody rb;
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float numberSpeed = 100;
+    float currentSpeed;
+    public Rigidbody Rb;
     void Start()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
+       Rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
+
+     void FixedUpdate()
+    {
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Rb.freezeRotation = false;
+            currentSpeed = numberSpeed;
+            Debug.Log("<color=green>Jump</color>");
+            Rb.AddTorque(Vector3.up * currentSpeed);
+
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            Rb.freezeRotation = false;
+            currentSpeed = numberSpeed;
+            Debug.Log("<color=green>Jump</color>");
+            Rb.AddTorque(Vector3.down * currentSpeed);
+        }
+        else
+        {
+            currentSpeed = 0;
+            Rb.freezeRotation = true;
+        }
+
+
+    }
+
     void Update()
     {
-        
+       
     }
 }
